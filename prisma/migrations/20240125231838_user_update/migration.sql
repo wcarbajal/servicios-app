702ad4d8f68b9305f8abf54bfb33e7501da41b3d
wcarbajal <wcarbajal@outlook.com>
2024-01-25 18:33:02 -0500

    ultima de la tarde

---
+++ prisma/migrations/20240125231838_user_update/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `name` on the `Usuarios` table. All the data in the column will be lost.
+  - Added the required column `nombreUsuario` to the `Usuarios` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Usuarios" DROP COLUMN "name",
+ADD COLUMN     "nombreUsuario" TEXT NOT NULL,
+ADD COLUMN     "password" TEXT;
---
+++ src/app/access/register/page.tsx
@@ -0,0 +1,8 @@
+
+export default function RegsiterPage() {
+  return (
+    <div>
+      <h1>Register Page</h1>
+    </div>
+  );
+}
